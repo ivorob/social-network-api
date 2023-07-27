87d71585cb780c0077b794d1b71b9f98fe36ab44
Ivan Vorobyev <ivorob@gmail.com>
2023-07-27 19:59:19 +0300

    Add packaging feature

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""Setup file for social-network-api."""
+import setuptools
+
+if __name__ == '__main__':
+    try:
+        setuptools.setup(setup_requires=['pbr'], pbr=True)
+    except:
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools "
+            "and wheel with:\n"
+            "    pip install -U setuptools wheel\n\n"
+        )
+        raise
---
+++ src/social_network_api/cli.py
@@ -0,0 +1,6 @@
+
+def run():
+    print('social-network-server')
+
+if __name__ == '__main__':
+    run()
